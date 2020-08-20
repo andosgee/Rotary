0ce5e810145212dafa41cfaf4894a1a744404e5a
Andrew <andrewgrant.grant1@gmail.com>
2020-08-20 18:28:49 +1200

    Added content to bott nav, with user checking.

--- inc/top_nav.php
+++ inc/top_nav.php
@@ -1,1 +1,19 @@
 <!-- Top nav -->
+<form method="get" name="search_site">
+  <input type="text" placeholder="Search" name="site_input" id="site_input">
+  <button name="site_button" id="site_button" type="submit">Go</button>
+</form>
+
+<?php
+if(empty($_SESSION['ID'])){
+  echo"
+  <a href='login.php'>Login</a>
+  |
+  <a href='signup.php'>Sign-Up</a>";
+}else {
+  $name = $_SESSION['NameF']." ".$_SESSION['NameS'];
+  echo "
+  Welcome ".$name."
+  <a href='logout.php'>Logout</a>";
+}
+?>
