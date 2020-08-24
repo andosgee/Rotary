9f0106f5c56a26992c8086942ff30e5267de925a
Andy <66146370+andosgee@users.noreply.github.com>
2020-08-24 19:08:38 +1200

    Added admin pages and links

---
+++ inc/admin_options.php
@@ -0,0 +1,36 @@
+<div class="admin_options">
+  <div class="admin_options" id="tile">
+    <h2>Reset User Passwords</h2>
+    <img alt="padlock_cross" src="">
+    <br>
+    <a href="password_reset.php">Click Here</a> to reset a members password.
+  </div>
+
+  <div class="admin_options" id="tile">
+    <h2>Create/Reinstate New User</h2>
+    <img alt="person plus" src="">
+    <br>
+    <a href="add_member.php">Click Here</a> to add a member.
+  </div>
+
+  <div class="admin_options" id="tile">
+    <h2>Remove User</h2>
+    <img alt="person minus" src="">
+    <br>
+    <a href="remove_user.php">Click Here</a> to remove a user.
+  </div>
+
+  <div class="admin_options" id="tile">
+    <h2>Moderate Stories</h2>
+    <img alt="article" src="">
+    <br>
+    <a href="mod_article.php">Click Here</a> to moderate/add an article.
+  </div>
+
+  <div class="admin_options" id="tile">
+    <h2>Create Newsletter</h2>
+    <img alt="newspaper" src="">
+    <br>
+    <a href="create_newsletter.php">Click Here</a> to create a newsletter.
+  </div>
+</div>
--- inc/default_options.php
+++ inc/default_options.php
@@ -1,3 +1,29 @@
 <div class="default_options">
-  
+  <div class="default_options" id="tile">
+    <h2>Change Password</h2>
+    <img alt="padlock" src="">
+    <br>
+    <a href="password_change.php">Click Here</a> to change your password.
+  </div>
+
+  <div class="default_options" id="tile">
+    <h2>Change Details</h2>
+    <img alt="person" src="">
+    <br>
+    <a href="details_change.php">Click Here</a> to change your details.
+  </div>
+
+  <div class="default_options" id="tile">
+    <h2>Create Story</h2>
+    <img alt="article" src="">
+    <br>
+    <a href="create_article.php">Click Here</a> to create an article.
+  </div>
+
+  <div class="default_options" id="tile">
+    <h2>Add Image</h2>
+    <img alt="picture" src="">
+    <br>
+    <a href="add_gallery_image.php">Click Here</a> to add an image to the site.
+  </div>
 </div>
