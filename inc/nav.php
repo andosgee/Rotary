6317ee058800b0288a8ed8264c4faf9a4cfd935e
typhon-h <harrison.tyson1@gmail.com>
2020-08-31 11:51:53 +1200

    Updated Nav

--- inc/nav.php
+++ inc/nav.php
@@ -1,47 +1,33 @@
 <div class="nav"> <!--Master Class of divs-->
-  <div  class="menu">
-    <div class="nav" id="items"><!--Holds nav items-->
-      <a href="index.php">Home</a>
-      <a href="about.php">About Us</a>
-      <a href="contact.php">Contact Us</a>
-      <a href="newsletter.php">Newsletters</a>
+  <div  class="nav__menu">
+    <div class="nav__items"><!--Holds nav items-->
+      <a class="nav__item" href="index.php">Home</a>
+      <a class="nav__item" href="about.php">About Us</a>
+      <a class="nav__item" href="contact.php">Contact Us</a>
+      <a class="nav__item" href="newsletter.php">Newsletters</a>
 
       <?php
       if(!empty($_SESSION['USER'])){ #Shows options avalible whether a user is signed in
-        $name = $_SESSION['NameF']." ".$_SESSION['NameS'];
         echo"
-        Welcome {$name}
-        <br>
-        <a href='minutes.php'>Minutes</a>
-        <br>
-        <a href='admin.php'>Member Options</a>
+        <a class=\"nav__item\" href='minutes.php'>Minutes</a>
+        <a class=\"nav__item\" href='admin.php'>Member Options</a>
         ";
       }else{
-        echo"<a href='signup.php'>Sign-Up</a>";
+        echo"<a class=\"nav__item\" href='signup.php'>Sign-Up</a>";
       }
       ?>
     </div>
-    <div class="sidebar">
-      <h3>Next Meeting</h3>
-      _Day of week_
-      <br>
-      Time: ???
-      <br>
-      Address: ???
-      <br>
-      <iframe>Google Maps</iframe>
-    </div>
+
+    <?php include './sidebar.php'; ?>
   </div>
-  <div class="login"> <!--Shows options avalible whether a user is signed in-->
     <?php
     if(empty($_SESSION['USER'])){
       echo"
-      <a href='login.php'>Login</a>";
+      <a class=\"nav__item\" href='login.php'>Login</a>";
     }else{
       echo "
-      <a href='logout.php'>Logout</a>";
+      <a class=\"nav__item\" href='logout.php'>Logout</a>";
     }
     ?>
 
-  </div>
 </div>
