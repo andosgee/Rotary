4f44d41e2864706eccf6dd60bd2550b184a74ecb
fronda-n <16frondan@students.stbedes.school.nz>
2020-09-01 12:41:05 +1200

    Title and styling

---
+++ inc/title.php
@@ -0,0 +1,6 @@
+<header class="title">
+  <div class="column">
+    <a class="title__link" href="index.php"><img class="title__logo" src="media/rotary_logo.png" alt="Rotary Logo"></a>
+    <h1 class="title__name"><b>ROTARY BELFAST</b></h1>
+  </div>
+</header>
