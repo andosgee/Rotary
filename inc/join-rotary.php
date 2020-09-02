efcaa58b6623296ad6f417107f992b44ff8bf8c9
typhon-h <harrison.tyson1@gmail.com>
2020-09-02 23:11:37 +1200

    Added content to index.php

---
+++ inc/join-rotary.php
@@ -0,0 +1,39 @@
+<div class="join-rotary column">
+  <div class="join-rotary__heading column">
+    <h2 class="join-rotary__title">Benefits of Joining Rotary</h2>
+    <span class="join-rotary__underline"></span>
+    <span class="join-rotary__underline"></span>
+    <span class="join-rotary__underline"></span>
+  </div>
+
+  <div class="column">
+    <div class="join-rotary__column row" onclick=(window.location.href='#')>
+      <img class="join-rotary__image" src="./media/rotary-join-template.png" alt="Image">
+      <div class="join-rotary__content column">
+        <h3 class="join-rotary__subheading">Reason</h3>
+        <p class="join-rotary__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, ipsa.</p>
+      </div>
+    </div>
+
+    <hr class="solid">
+
+    <div class="join-rotary__column row" onclick=(window.location.href='#')>
+      <img class="join-rotary__image" src="./media/rotary-join-template.png" alt="Image">
+      <div class="join-rotary__content column">
+        <h3 class="join-rotary__subheading">Reason</h3>
+        <p class="join-rotary__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, ipsa.</p>
+      </div>
+    </div>
+
+    <hr class="solid">
+    
+    <div class="join-rotary__column row" onclick=(window.location.href='#')>
+      <img class="join-rotary__image" src="./media/rotary-join-template.png" alt="Image">
+      <div class="join-rotary__content column">
+        <h3 class="join-rotary__subheading">Reason</h3>
+        <p class="join-rotary__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, ipsa.</p>
+      </div>
+    </div>
+
+  </div>
+</div>
