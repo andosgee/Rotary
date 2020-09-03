2b722e80b66c658f0a40a0092d2492df909e6e48
typhon-h <harrison.tyson1@gmail.com>
2020-09-03 14:19:36 +1200

    Added footer

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+<footer class="footer column">
+  <a href="#" class="footer__top">Return to Top</a>
+
+  <form method="POST" class="footer__newsletter column">
+    <label for="footer_input" class="footer__label">Subscribe to Our Newsletter</label>
+    <div class="footer__inputs row">
+      <input type="text" class="footer__text" name="footer_input" placeholder="Email...">
+      <input type="submit" class="footer__submit">
+    </div>
+  </form>
+
+  <div class="footer__info column">
+    <a class="footer__legal" href="#">Privacy Policy</a>
+    <p class="footer__legal">©2020 Rotary Club Belfast. All rights reserved.</p>
+    <p class="footer__legal">Developed by students at St. Bede's College</p>
+
+  </div>
+
+</footer>
