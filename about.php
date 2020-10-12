2ebc8e6e6bd0d5a4c600682eaddcaea9aafc89a5
typhon-h <harrison.tyson1@gmail.com>
2020-10-13 11:39:03 +1300

    Sidebar for desktop

--- about.php
+++ about.php
@@ -4,7 +4,15 @@
 <body>
   <?php include './inc/title.php'; ?>
   <?php include "./inc/nav.php"; ?>
-  <?php include "./inc/about_content.php"; ?>
+  <div class="row">
+    <div class="column sidebar-d">
+          <?php include './inc/sidebar.php'; ?>
+        </div>
+    <div class="column">
+      <?php include "./inc/about_content.php"; ?>
+    </div>
+  </div>
+
   <?php include './inc/footer.php'; ?>
 </body>
 </html>
