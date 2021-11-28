2f8ebc8fdd96a4e325851aef1b27a260681de9b6
Andy <66146370+andosgee@users.noreply.github.com>
2021-11-28 21:15:50 +1300

    Minor cosmetic changes to home page

--- inc/join-rotary.php
+++ inc/join-rotary.php
@@ -6,8 +6,31 @@
   </div>
 
   <div class="join-rotary__content column">
+    <div class="join-rotary__column row" onclick=(window.location.href='fundraisers.php')>
+      <img class="join-rotary__image" src="./media/icons/fundraising.png" alt="Image">
+      <div class="join-rotary__content column">
+        <h3 class="join-rotary__subheading">Fundraising</h3>
+        <p class="join-rotary__desc">As a club, we fundraise during the year for many and varied causes
+                            and projects. We are currently running our potato fundraiser, the funds from
+                            this go towards our community projects. Targeting youth, schools, Pacific projects
+                          and Foodbank Canterbury</p>
+      </div>
+    </div>
+
+    <hr class="solid">
+
+    <div class="join-rotary__column row" onclick=(window.location.href='contact.php')>
+      <img class="join-rotary__image" src="./media/icons/contact.png" alt="Image">
+      <div class="join-rotary__content column">
+        <h3 class="join-rotary__subheading">Contact Us</h3>
+        <p class="join-rotary__desc">Got some questions? Don't hesitate to get in touch.</p>
+      </div>
+    </div>
+
+    <hr class="solid">
+
     <div class="join-rotary__column row" onclick=(window.location.href='about.php')>
-      <img class="join-rotary__image" src="./media/rotary-join-template.png" alt="Image">
+      <img class="join-rotary__image" src="./media/icons/about.png" alt="Image">
       <div class="join-rotary__content column">
         <h3 class="join-rotary__subheading">About Us</h3>
         <p class="join-rotary__desc">We originally established as the Rotary Club of Belfast in 1969 before
@@ -16,30 +39,6 @@
                                     The Club is well known for its “friendly vibe” with many members forging
                                     lasting friendships. Social Activities are encouraged and well patronised throughout the year.
 </p>
-      </div>
-    </div>
-
-    <hr class="solid">
-
-    <div class="join-rotary__column row" onclick=(window.location.href='contact.php')>
-      <img class="join-rotary__image" src="./media/rotary-join-template.png" alt="Image">
-      <div class="join-rotary__content column">
-        <h3 class="join-rotary__subheading">Contact Us</h3>
-        <p class="join-rotary__desc">New members are especially welcomed. If you have some spare time,
-                                    and think Rotary may interest you, please contact us on 0274633644
-                                    for further information.</p>
-      </div>
-    </div>
-
-    <hr class="solid">
-
-    <div class="join-rotary__column row" onclick=(window.location.href='')>
-      <img class="join-rotary__image" src="./media/rotary-join-template.png" alt="Image">
-      <div class="join-rotary__content column">
-        <h3 class="join-rotary__subheading">Fundraising</h3>
-        <p class="join-rotary__desc">As a club, we fundraise during the year for many and varied causes
-                                    and projects. We can, and do, assist our community both locally, and
-                                    internationally with a particular focus on assisting Youth.</p>
       </div>
     </div>
 
