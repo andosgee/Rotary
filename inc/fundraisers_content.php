714995d59d7ab9be556a61f81f17bca2e843f79c
Andy <66146370+andosgee@users.noreply.github.com>
2021-11-28 23:33:39 +1300

    Completed fundraiser page

--- inc/fundraisers_content.php
+++ inc/fundraisers_content.php
@@ -6,14 +6,52 @@
     <span class="underline"></span>
   </div>
   <div class="about__main">
-    <h2 class="about__title">Potato Fundraiser (Date valid goes here)</h2>
+    <h2 class="about__title">Potato Fundraiser - Selling between 9th December to 24th December</h2>
     <p class="info">
-      At Rotary we do potato etc. Extra info to .............................
-      .......................................................................................
-      .......................................................................................
-      .......................................................................................
+      At Rotary, we have started our annual potato fundraiser. This is a key fundraiser for our club as it bings in
+      <b>$70,000</b> for us to put towards community projects that can benefiet schools, charities and communities.
       <br>
-      <img class="about__image" src="./media/potato_fundraiser.jpg" alt="Potato Funraiser Image">
+      <p>
+      <b>History:</b> We have been growing, harvesting and selling delicious new Cliff Kidney potatoes at Christmas since 2012.
+      We have club members who have the expertise and machinery to enable this project to be the success it has been.
+      As well, all members can contribute in some way, whether it is in the growing, packing or selling.
+      The potatoes are now becoming very well known around town and we have many queries each year as to when will they be available.
+      We sell several thousand of the 2kg boxes each year and all proceeds go to charity.
+    </p>
+      <br>
+      <img class="about__image" src="./media/potato_fundraiser.jpg" alt="Potato Fundraiser Image">
+    <p>
+      <b>Price:</b> We sell them in distinctive blue 2kg boxes for $6/box.
+    </p>
+    <br>
+    <img class="about__image" src="./ass/gallery/618a5f4add5292.30668835.jpg" alt="Potato box Image">
+    <p>
+      <b>When and Where to Buy:</b> We will have stalls at the following locations: 587 Marshland Road; Beside the potato paddock at
+      751 Marshland Road; 216 Cavendish Rd; cnr Main North Road and Factory Road, Belfast; and at the Kaiapoi Golf Club,
+      Williams St, Kaiapoi. We will also be at Ohoka Market on the Fridays of December 10, December 17, and possibly 24th December; Riccarton
+      Market December 12 and 19th December; and Burwood hospital on December 9, December 16 and 23rd December. Club members will also be selling
+      to their own contacts.
+    </p>
+    <p>
+      <b>Funds:</b>We support many local and wider charities and organisations, with an emphasis on youth.
+      Among many donations in the past year, we have supported the following local causes:
+      <br>
+      <ul>
+        <li>Computers to two local schools</li>
+        <li>Belfast Community Network</li>
+        <li>Belfast Netball</li>
+        <li>Coastgaurd North Canterbury</li>
+        <li>Northcote School</li>
+        <li>St John</li>
+        <li>North Canterbury Alpine Trust</li>
+        <li>Rotary Youth Leadership Award</li>
+        <li>Rotary Youth Driver Awareness</li>
+        <li>Foodbank Canterbury</li>
+      </ul>
+      In the past the money raised has also helped to relieve disaster struck areas such-as the Weesport Flood Relief, Projects in the Pacific.
+      <br>
+
+    </p>
     </p>
   </div>
 </div>
