d607798784c0fac6b901581c430e299ba0192b27
Andy <66146370+andosgee@users.noreply.github.com>
2022-01-28 16:19:50 +1300

    Completed search overhaul

--- inc/search.php
+++ inc/search.php
@@ -1,7 +1,9 @@
 <form name="user_search" class="form" method="post">
   <div class="row">
-  <input class="form__input" name="user_search_input" placeholder="Search......" autofocus>
-  
-  <button type="submit" name="user_search_button" class="form__button form__button--submit">Search</button>
-</div>
+    <label for="user_search_input">Search:</label>
+    <input class="form__input" name="user_search_input" placeholder="Search......" autofocus>
+  </div>
+  <div class="row">
+    <button type="submit" name="user_search_button" class="form__button form__button--submit">Search</button>
+  </div>
 </form>
