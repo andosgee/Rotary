35c23b31cbb4a23a0f145977ff8361222fcf2d4b
Andy <66146370+andosgee@users.noreply.github.com>
2022-01-31 19:09:36 +1300

    ddslick init

--- inc/approve_images.php
+++ inc/approve_images.php
@@ -4,19 +4,30 @@
 
 
 ?>
+<script>
+  $(document).ready(function(){
+    $('#article_select').ddslick(); // Turn on Slickness
+  });
+</script>
 
 <form method="POST" name="create_article_form" class="form column" enctype="multipart/form-data">
   <div class="column form__head">
     <h1 class="form__title">Moderate Images</h1>
     <div class="row">
       <label class="form__label" for="user_select">Manage/Approve Images:</label>
-      <select name="article_select" class="form__select" onchange="populateFormByID(this.value,'./inc/moderate_form_image.php')">
+      <select name="article_select" class="form__select" id='article_select'>
         <option value="" selected>-</option>
         <?php
           $images = get_from_table('tbl_gallery',"`GID` IS NOT NULL","`tbl_gallery`.`Moderated`");
 
           foreach($images as $image){
-            echo "<option value=\"{$image['GID']}\">{$image['ImageName']}</option>";
+            $published = "";
+            if($image['Moderated'] = 1){
+              $published = "Apporved";
+            }else{
+              $published = "Awaiting Approval";
+            }
+            echo "<option value=\"{$image['GID']}\" data-imagesrc='{$image['ImageName']}'>{$published}</option>";
           } ?>
       </select>
     </div>
--- inc/head.php
+++ inc/head.php
@@ -22,14 +22,19 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> <!--Ajax Jquery Lib-->
   <script type="text/javascript" src="./chosen_lib/chosen.jquery.js"></script> <!--Jquery full-->
   <script type="text/javascript" src="./chosen_lib/chosen.jquery.min.js"></script> <!--Jquery Min-->
-
+  <!--DDSLick things-->
+  <script type="text/javascript" src="https://cdn.rawgit.com/prashantchaudhary/ddslick/master/jquery.ddslick.min.js" ></script>
+  <script type="text/javascript" src="./ddslick/jquery.ddslick.js"></script> <!--Jquery full-->
+  <script type="text/javascript" src="./ddslick/jquery.ddslick.min.js"></script> <!--Jquery Min-->
   <!-- Dynamic Title - based off page name -->
   <title><?php echo ucwords(get_active_page()); ?> | Belfast-Kaiapoi Rotary</title>
   <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PSFR2KS');</script>
+  <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PSFR2KS');
+  </script>
 <!-- End Google Tag Manager -->
 </head>
